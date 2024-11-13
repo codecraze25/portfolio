e090ec0bc718a9a51eefdeac60c309fea13520e2
codecraze25 <juanhub25@outlook.com>
2024-11-13 07:45:46 +0100

    updates

---
+++ lib/data.ts
@@ -0,0 +1,232 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import hulkCars from "@/public/hulk-cars.png";
+import scribbleImg from "@/public/scribble.png";
+import fjallravenImg from "@/public/fjallraven.png";
+import juicedbikesImg from "@/public/juicedbikes.png";
+import columbiaImg from "@/public/columbia.png";
+import albertsonImg from "@/public/albertson.png";
+
+import ozdevsImg from "@/public/ozdevs-two.png";
+import assetTradingImg from "@/public/asset-trading.jpg";
+import euphorusImg from "@/public/euphorus-two.png";
+import euphorusBackendImg from "@/public/euphorus-backend.png";
+import techprowlImg from "@/public/techprowl-two.png";
+import taskManagerImg from "@/public/task-manager.png";
+import familyTreeImg from "@/public/bfsproject.jpg";
+import binaryGameImg from "@/public//binary-game.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Education",
+    hash: "#education",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Senior Full Stack Developer",
+    location: "Devsinc - Contract",
+    description:
+      "Implemented React Native for cross-platform apps and contributed to full-stack development using technologies like JavaScript, Node.js, and Python. I also developed RESTful APIs, conducted code reviews, and promoted collaboration using tools like Git, Jira, and Trello.",
+    icon: React.createElement(CgWorkAlt),
+    date: "April 2024 - Present",
+  },
+  {
+    title: "Senior Software Engineer",
+    location: "Columbia Sportswear",
+    description:
+      "Collaborated with a team to design scalable web apps using React, Next.js, and Angular, while developing robust backend systems with Node.js, Java, and Spring Boot. Leveraged AWS, serverless architecture, and real-time data streaming with Apache Kafka, alongside Kubernetes and Docker for efficient deployment.",
+    icon: React.createElement(CgWorkAlt),
+    date: "February 2022 - March 2024",
+  },
+  {
+    title: "Software Engineer",
+    location: "Albertsons",
+    description:
+      "Worked on both frontend and backend using PHP (Laravel), TypeScript (Vue, React, Node.js), and integrated payment gateways like PayPal and Stripe. Also implemented SSR for SEO optimization, used Jest/Mocha for testing, and built a Python/Django backend with AI algorithms, integrating Elasticsearch for efficient search functionality.",
+    icon: React.createElement(CgWorkAlt),
+    date: "January 2018 - January 2022",
+  },
+  {
+    title: "Full Stack Developer",
+    location: "Uber",
+    description:
+      "Developed dynamic front-end applications with Vue, React, Tailwind CSS, and JavaScript for responsive UI, and built LAMP stack web apps using Laravel and Symfony. Also utilized jQuery for DOM manipulation and AJAX, while enhancing code quality through unit and integration testing with Jest and PHPUnit.",
+    icon: React.createElement(CgWorkAlt),
+    date: "September 2016 - December 2017",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Scribble",
+    description:
+      "I built this project using React. Scribble Together offers a collaborative online whiteboard, ideal for remote tutoring, meetings, and shared note-taking. It supports handwriting, diagram drawing, PDF markup, and works well with iPads and Apple Pencils.",
+    tags: ["TypeScript", "Tailwind CSS", "React", "Stripe"],
+    icons: [
+      "devicon:typescript",
+      "logos:react",
+      "devicon:tailwindcss",
+      "logos:stripe",
+    ],
+    imageUrl: scribbleImg,
+    urlLink: "https://scribbletogether.com/",
+  },
+  {
+    title: "FJALL RAVEN",
+    description:
+      "I built this project using React. Fjällräven is a Swedish outdoor brand known for its durable, functional, and eco-friendly products, including backpacks, clothing, and accessories. The company emphasizes sustainability, using materials like organic cotton and recycled polyester.",
+    tags: ["TypeScript", "React", "Node.js", "AWS"],
+    icons: [
+      "logos:aws",
+      "devicon:typescript",
+      "logos:react",
+      "logos:bootstrap",
+      "logos:nodejs-icon",
+    ],
+    imageUrl: fjallravenImg,
+    urlLink: "https://www.fjallraven.com/us/en-us/",
+  },
+  {
+    title: "Juiced Bikes",
+    description:
+      "I built this project using React. Hulk Cars offers car rental services, likely with a focus on integrating blockchain technology for vehicle tokenization. This approach may allow fractional ownership or secure transactions involving car assets.",
+    tags: ["TypeScript", "React", "Node.js", "AWS"],
+    icons: ["logos:shopify"],
+    imageUrl: juicedbikesImg,
+    urlLink: "https://www.juicedbikes.com/",
+  },
+  {
+    title: "Hulk Cars",
+    description:
+      "I built this project using React. Hulk Cars offers car rental services, likely with a focus on integrating blockchain technology for vehicle tokenization. This approach may allow fractional ownership or secure transactions involving car assets.",
+    tags: ["TypeScript", "React", "Node.js", "AWS"],
+    icons: [
+      "logos:aws",
+      "devicon:typescript",
+      "logos:react",
+      "logos:bootstrap",
+      "logos:nodejs-icon",
+    ],
+    imageUrl: hulkCars,
+    urlLink: "https://hulkcars.com/",
+  },
+  {
+    title: "Columbia Sportswear",
+    description:
+      "Columbia Sportswear's website serves as the central platform for customers to explore and purchase outdoor apparel, footwear, and equipment. The website is designed with a focus on providing a seamless, user-friendly shopping experience that allows customers to browse by category, activity, or new arrivals. It features sections for men, women, and kids, as well as specialized collections like hiking, fishing, and skiing gear. The site also offers a membership program, allowing users to earn rewards and access exclusive discounts. Additionally, it functions as a hub for customer support, store locator services, and Columbia’s sustainability initiatives, showcasing the brand’s commitment to responsible manufacturing and outdoor preservation.",
+    tags: ["Java", "MariaDB", "Swing"],
+    icons: ["logos:java", "logos:mariadb-icon"],
+    imageUrl: columbiaImg,
+    urlLink: "https://www.columbia.com/",
+  },
+  {
+    title: "Albertson",
+    description:
+      "I built this project using React. Hulk Cars offers car rental services, likely with a focus on integrating blockchain technology for vehicle tokenization. This approach may allow fractional ownership or secure transactions involving car assets.",
+    tags: ["TypeScript", "React", "Node.js", "AWS"],
+    icons: ["logos:shopify"],
+    imageUrl: albertsonImg,
+    urlLink: "https://www.albertsons.com/",
+  },
+] as const;
+
+export const skillsData = [
+  {
+    name: "JavaScript",
+    icon: "logos:javascript",
+  },
+  {
+    name: "TypeScript",
+    icon: "logos:typescript-icon",
+  },
+  {
+    name: "React",
+    icon: "logos:react",
+  },
+  {
+    name: "Node.js",
+    icon: "logos:nodejs-icon",
+  },
+  {
+    name: "PHP",
+    icon: "logos:php",
+  },
+  {
+    name: "Laravel",
+    icon: "logos:laravel",
+  },
+  {
+    name: "Symfony",
+    icon: "logos:symfony",
+  },
+  {
+    name: "Python",
+    icon: "logos:python",
+  },
+  {
+    name: "Java",
+    icon: "logos:java",
+  },
+  {
+    name: "HTML",
+    icon: "vscode-icons:file-type-html",
+  },
+  {
+    name: "CSS",
+    icon: "vscode-icons:file-type-css",
+  },
+  {
+    name: "SCSS",
+    icon: "logos:sass",
+  },
+  {
+    name: "Git",
+    icon: "logos:git-icon",
+  },
+  {
+    name: "MySQL",
+    icon: "logos:mysql",
+  },
+  {
+    name: "Linux",
+    icon: "flat-color-icons:linux",
+  },
+  {
+    name: "AWS",
+    icon: "logos:aws",
+  },
+  {
+    name: "Docker",
+    icon: "logos:docker-icon",
+  },
+  {
+    name: "Jenkins",
+    icon: "devicon:jenkins",
+  },
+  {
+    name: "Shopify",
+    icon: "logos:shopify",
+  },
+] as const;
